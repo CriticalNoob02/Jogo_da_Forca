d1b7e061f8a8f2342e468224bdecb9d57b7d4b14
CriticalNoob02 <103211486+CriticalNoob02@users.noreply.github.com>
2022-08-07 19:04:53 -0300

    Quase_Finalizando

--- Atv_8.py
+++ Atv_8.py
@@ -1,97 +1,154 @@
-"""
-
-tarefa1 = []
-concluir = True
-
-while concluir:
-    atv = input("Descreva a atividade: ")
-
-    tarefa1.append(atv)
-
-    print(f"Sua próxima atividade é: {tarefa1[-1]}")
-    finalizar = int(input("Voce deseja finalizar as atividades? (S=1 / N=2) "))
-    if(finalizar == 1):
-        concluir = False
-    else:
-        pass    
-
-print(f"Parabens, você conclui suas tarefas.")
-for i in tarefa1:
-    print(i)
-
-"""
-
-"""
-
-## Tabuada Personalizada
-
-num1 = int(input("Digite o numero que deseja multiplicar: "))
-qtd = int(input("Digite a Quantidade de vezes que deseja multiplicar: ")) 
-inicio = int(input("Digite o númwero que deseja iniciar a multiplicação: ")) 
-inicio1 = ""
-soma = inicio+qtd
-tabuada = []
-confirmação = True
-
-
-
-while confirmação:
-    
-    if(inicio1 == ""):
-        inicio1 = inicio
-        tabuada.append(inicio1)
-
-    elif( inicio1 == soma):
-        confirmação = False
-
-    else:
-        inicio1 += 1
-        tabuada.append(inicio1)
-
-for i in tabuada:
-    print(f"{num1} X {i} = {num1*i}")
-
-print(tabuada)    
-
-"""
-
 ## Jogo da Forca;
 
-resposta = []
-tentativas = []
-acertos = []
-palavra = input("Digite a palavra do jogo: ")
-palavra = palavra.upper().strip()
+## Criação das Variaveis;
+resposta = [] ## Palavra Respostas
+tentativas = [] ## Mostrar as letras Usadas
+acertos = [] ## Mostrar a Respostas
 vida = 6
 laço = True
 
+## Entrada e Formatação dos Dados;
+palavra = input("Digite a palavra do jogo: ") 
+dick = input("Digite uma dica para sua palavra: ")
+palavra = palavra.upper().strip()
+dick = dick.strip().capitalize()
+
+## Espaço vaziu
+print("\n \n \n \n \n \n \n \n \n")
+print("\n \n \n \n \n \n \n \n \n")
+print("\n \n \n \n \n \n \n \n \n")
+print("\n \n \n \n \n \n \n \n \n")
+
+## Soletrar a lista.
 for i in palavra:
-    print(i)
     resposta += i
 
-print(resposta)
+## Loop para Adicionar a quantidade de letras no ACERTOS.
+for i in resposta:
+    acertos.append("_")
+
+## Inicio do Jogo;
+print("####################### Python Da Forca #########################################")
+print(" ")
+
+## Exibindo Quantidade de letras na resposta
+print(f"A palavra secreta é: {acertos}")  
+
+## Dica da palavra Secreta
+print(" ")
+print(f"Segue a DICA da palvra secreta abaixo:")
+print(dick)
+print("\n \n \n \n")
+
+## Loop do Jogo.
 while laço:
-    print(acertos)
+    num = 0
     letra = input("Digite a Letra: ")
     letra = letra.upper().strip()
     confirmação = False
     tentativas.append(letra)
-    print(tentativas)
-    print(" ")
+    print(" ")  
 
+    ## Loop para adicionar letras corretas;
     for i in resposta:
         if(letra == i):
-            print("Acertou")
-            acertos.append(letra)
+            acertos[num] = letra
+            num +=1
             confirmação = True
         else:
-            acertos.append("_")
-            
+            num += 1
+            pass   
+
+    ## Exibindo Acertos e Tentativas
+    print(f"A palavra é: {acertos}")  
+    print(" ")
+    print(f"Você utilizou essas letras até agora: {tentativas}")
+
+    ## Perdendo Vidas e mensagem de Pontuação;
     if not confirmação:
         vida -= 1
-        print("Errou")
-        print(vida)
+        print(" ")
+        print("Você Errou!!")
+        print(f"VIdas: {vida}")
+    else:
+        print(" ")
+        print("Você Acertou!!")  
+        print(f"VIdas: {vida}") 
 
+    ## Desenhos;
+    if(vida == 6):
+         print("  _______   ")
+         print("  |     |   ")
+         print("  |         ")
+         print("  |         ")
+         print("  |         ")
+         print("  |         ")
+         print("  |         ")
+         print("=====       ")
+
+    elif(vida == 5):
+         print("  _______   ")
+         print("  |     |   ")
+         print("  |     0   ")
+         print("  |         ")
+         print("  |         ")
+         print("  |         ")
+         print("  |         ")
+         print("=====       ")
+
+    elif(vida == 4):
+         print("  _______   ")
+         print("  |     |   ")
+         print("  |     0   ")
+         print("  |     |   ")
+         print("  |     |   ")
+         print("  |         ")
+         print("  |         ")
+         print("=====       ")
+
+    elif(vida == 3):
+         print("  _______   ")
+         print("  |     |   ")
+         print("  |     0   ")
+         print("  |    /|   ")
+         print("  |     |   ")
+         print("  |         ")
+         print("  |         ")
+         print("=====       ")
+
+    elif(vida == 2):
+         print("  _______   ")
+         print("  |     |   ")
+         print("  |     0   ")
+         print("  |    /|\  ")
+         print("  |     |   ")
+         print("  |         ")
+         print("  |         ")
+         print("=====       ")
+
+    elif(vida == 1):
+         print("  _______   ")
+         print("  |     |   ")
+         print("  |     0   ")
+         print("  |    /|\  ")
+         print("  |     |   ")
+         print("  |      \  ")
+         print("  |         ")
+         print("=====       ")
+
+    elif(vida == 0):
+         print("  _______   ")
+         print("  |     |   ")
+         print("  |     0   ")
+         print("  |    /|\  ")
+         print("  |     |   ")
+         print("  |    / \  ")
+         print("  |         ")
+         print("=====       ")
+
+    ## Ganhou ou Perdeu;
     if(vida == 0):
-         print("Ixxxii, você acabou de se enforcar!! ")  
+         print("Ixxxii, o feudo te Enforcou!! ") 
+         print("\n \n \n \n") 
          laço =  False
+    elif(acertos == resposta):
